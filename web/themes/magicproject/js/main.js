92297b49f57f5ecf1b47c2e83fe0f27b5b9ecbba
Unknown <david2205@orange.fr>
2018-06-19 22:54:32 +0200

    create template & css for sliders

--- web/themes/magicproject/js/main.js
+++ web/themes/magicproject/js/main.js
@@ -1,3 +1,20 @@
 (function($) {
 
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav',
+        adaptiveHeight: true
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
 })( jQuery );

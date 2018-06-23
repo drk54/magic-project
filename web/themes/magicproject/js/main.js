414dd9cb24aaec45311cbe16862d1e410459806b
Unknown <david2205@orange.fr>
2018-06-23 11:51:31 +0200

    update slider nav & slider equal height

--- web/themes/magicproject/js/main.js
+++ web/themes/magicproject/js/main.js
@@ -6,7 +6,8 @@
         arrows: false,
         fade: true,
         asNavFor: '.slider-nav',
-        adaptiveHeight: true
+        adaptiveHeight: true,
+        lazyLoad: 'ondemand'
     });
     $('.slider-nav').slick({
         slidesToShow: 3,
@@ -14,7 +15,47 @@
         asNavFor: '.slider-for',
         dots: false,
         centerMode: true,
-        focusOnSelect: true
+        focusOnSelect: true,
+        vertical: true,
+        lazyLoad: 'ondemand'
     });
+
+    $(window).on('rezise', function(){
+        var max_height = $('.slider-for').height();
+        $('.slider-nav').css('max-height', max_height+'px');
+    });
+
+    $(window).ready(function(){
+        var max_height = $('.slider-for').height();
+        $('.slider-nav').css('max-height', max_height+'px');
+    });
+
+    const $slider = $(".slider-nav");
+    $slider
+        .on('init', function() {
+            mouseWheel($slider)
+        })
+        .slick({
+            dots: true,
+            vertical: true,
+            infinite: false,
+        });
+        function mouseWheel($slider) {
+            $(window).on('wheel', { $slider: $slider }, mouseWheelHandler)
+        }
+        function mouseWheelHandler(event) {
+            event.preventDefault();
+            const $slider = event.data.$slider
+            const delta = event.originalEvent.deltaY
+            if(delta > 0) {
+                $slider.slick('slickNext')
+            }
+            else {
+                $slider.slick('slickPrev')
+            }
+        }
+
+
+
 
 })( jQuery );

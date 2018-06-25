e6736a3493a4433979f497c46b5c1710dd9435b9
Unknown <david2205@orange.fr>
2018-06-25 21:32:14 +0200

    add overlay for slider

--- web/themes/magicproject/js/main.js
+++ web/themes/magicproject/js/main.js
@@ -1,7 +1,12 @@
 (function($) {
 
-    const $slider_for = $(".slider-for");
-    const $slider_nav = $(".slider-nav");
+    //
+    //SLIDER
+    //
+
+    const $slider_for   = $(".slider-for");
+    const $slider_nav   = $(".slider-nav");
+    const $full_screen  = $('.full_screen');
 
     $slider_for.slick({
         slidesToShow: 1,
@@ -31,6 +36,32 @@
     $(window).ready(function(){
         var max_height = $slider_for.height();
         $slider_nav.css('max-height', max_height+'px');
+    });
+
+    var image_index = false;
+
+    $('.slider-for img').on('click', function(){
+        image_index = $(this).data('slick-index');
+        var image = $('.full_screen__data[data-index='+image_index+']');
+        var img_src = image.data('src');
+        $full_screen.toggleClass('visible');
+        $full_screen.append('<img class="full_screen__image" src="'+img_src+'">');
+    });
+
+    $('.full_screen__close').on('click', function () {
+        if(image_index !== false){
+            $('.full_screen__image').remove();
+            $full_screen.toggleClass('visible');
+            image_index = false;
+        }
+    });
+
+    $(window).on('keyup', function(e){
+        if(image_index !== false && e.keyCode === 27){
+            $('.full_screen__image').remove();
+            $full_screen.toggleClass('visible');
+            image_index = false;
+        }
     });
 
 })( jQuery );
